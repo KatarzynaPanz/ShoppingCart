ae59daa266ebae6cb94af1846e681544c303ac9b
Katarzyna Panz <katarzyna.panz@gmail.com>
2023-08-25 14:22:05 +0200

    Add changes to Footer and Header and add useCart component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,25 @@
+import useCart from "../hooks/useCart";
+
 type PropsType = {
   viewCart: boolean;
 };
 
 const Footer = ({ viewCart }: PropsType) => {
-  return <div>Footer</div>;
+  const { totalItems, totalPrice } = useCart();
+
+  const year: number = new Date().getFullYear();
+
+  const pageContent = (
+    <footer>
+      <div className="footer">
+        <div>
+          <p>Total Items: {totalItems}</p>
+          <p>Total Price: {totalPrice} </p>
+        </div>
+      </div>
+    </footer>
+  );
+  return pageContent;
 };
 
 export default Footer;
