b288524b29ff3c0356bc4ac5a9a8d1c91da21198
Katarzyna Panz <katarzyna.panz@gmail.com>
2023-08-28 12:12:44 +0200

    Add changes to Product component

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,4 +1,15 @@
-const Product = () => {
+import { ProductType } from "../context/ProductsProvider";
+import { ReducerActionType, ReducerAction } from "../context/CartProvider";
+import { ReactElement } from "react";
+
+type PropsType = {
+    product: ProductType,
+    dispatch: React.Dispatch<ReducerAction>,
+    REDUCER_ACTIONS: ReducerActionType,
+    inCart: boolean
+}
+
+const Product = ({product, dispatch, REDUCER_ACTIONS, inCart} :PropsType):ReactElement => {
     return (
         <div>Product</div>
     )
